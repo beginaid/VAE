97736f097007eefbca4c4fefa5e5e19620e3ef63
beginaid <zuka.info.official@gmail.com>
2022-01-08 12:17:12 +0900

    [modify] English translation of comments

--- models/VAE.py
+++ models/VAE.py
@@ -3,41 +3,42 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class VAE(nn.Module):
     def __init__(self, z_dim):
-        """コンストラクタ
+        """Constructor
 
         Args:
-            z_dim (int): 潜在空間の次元数
+            z_dim (int): Dimensions of the latent variable.
 
         Returns:
             None.
 
         Note:
-            eps (float): オーバーフローとアンダーフローを防ぐための微小量
+            eps (float): Small amounts to prevent overflow and underflow.
         """
-        super(VAE, self).__init__() # VAEクラスはnn.Moduleを継承しているため親クラスのコンストラクタを呼ぶ必要がある
-        self.eps = np.spacing(1) # オーバーフローとアンダーフローを防ぐための微小量
-        self.x_dim = 28 * 28 # MNISTの場合は28×28の画像であるため
-        self.z_dim = z_dim # インスタンス化の際に潜在空間の次元数は自由に設定できる
-        self.enc_fc1 = nn.Linear(self.x_dim, 400) # エンコーダ1層目
-        self.enc_fc2 = nn.Linear(400, 200) # エンコーダ2層目
-        self.enc_fc3_mean = nn.Linear(200, z_dim) # 近似事後分布の平均
-        self.enc_fc3_logvar = nn.Linear(200, z_dim) # 近似事後分布の分散の対数
-        self.dec_fc1 = nn.Linear(z_dim, 200) # デコーダ1層目
-        self.dec_fc2 = nn.Linear(200, 400) # デコーダ2層目
-        self.dec_drop = nn.Dropout(p=0.2) # 過学習を防ぐために最終層の直前にドロップアウト
-        self.dec_fc3 = nn.Linear(400, self.x_dim) # デコーダ3層目
+        super(VAE, self).__init__()
+        self.eps = np.spacing(1)
+        self.x_dim = 28 * 28 # The image in MNIST is 28×28
+        self.z_dim = z_dim
+        self.enc_fc1 = nn.Linear(self.x_dim, 400)
+        self.enc_fc2 = nn.Linear(400, 200)
+        self.enc_fc3_mean = nn.Linear(200, z_dim)
+        self.enc_fc3_logvar = nn.Linear(200, z_dim)
+        self.dec_fc1 = nn.Linear(z_dim, 200)
+        self.dec_fc2 = nn.Linear(200, 400)
+        self.dec_drop = nn.Dropout(0.2)
+        self.dec_fc3 = nn.Linear(400, self.x_dim)
 
     def encoder(self, x):
-        """エンコーダ
+        """Encoder
 
         Args:
-            x (torch.tensor): (バッチサイズ, 入力次元数)サイズの入力データ
+            x (torch.tensor): Input data whose size is (Batch size, x_dim).
 
         Returns:
-            mean (torch.tensor): 近似事後分布の平均
-            logvar (torch.tensor): 近似事後分布の分散の対数
+            mean (torch.tensor): Mean value of approximated posterior distribution whose size is (Batch size, z_dim).
+            logvar (torch.tensor): Log-variance of approximated posterior distribution (Batch size, z_dim).
         """
         x = x.view(-1, self.x_dim)
         x = F.relu(self.enc_fc1(x))
@@ -45,27 +46,27 @@ class VAE(nn.Module):
         return self.enc_fc3_mean(x), self.enc_fc3_logvar(x)
 
     def sample_z(self, mean, log_var, device):
-        """Reparametrization trickに基づく潜在変数Zの疑似的なサンプリング
+        """Sampling latent variables using reparametrization trick
 
         Args:
-            mean (torch.tensor): 近似事後分布の平均
-            logvar (torch.tensor): 近似事後分布の分散の対数
-            device (String): GPUが使える場合は"cuda"でそれ以外は"cpu"
+            mean (torch.tensor): Mean value of approximated posterior distribution whose size is (Batch size, z_dim).
+            logvar (torch.tensor): Log-variance of approximated posterior distribution (Batch size, z_dim).
+            device (String): "cuda" if GPU is available, or "cpu" otherwise.
 
         Returns:
-            z (torch.tensor): (バッチサイズ, z_dim)サイズの潜在変数
+            z (torch.tensor): Latent variable whose size is (Batch size, z_dim).
         """
         epsilon = torch.randn(mean.shape, device=device)
         return mean + epsilon * torch.exp(0.5 * log_var)
 
     def decoder(self, z):
-        """デコーダ
+        """Decoder
 
         Args:
-            z (torch.tensor): (バッチサイズ, z_dim)サイズの潜在変数
+            z (torch.tensor): Latent variable whose size is (Batch size, z_dim).
 
         Returns:
-            y (torch.tensor): (バッチサイズ, 入力次元数)サイズの再構成データ
+            (torch.tensor): Reconstruction data whose size is (batch size, x_dim).
         """
         z = F.relu(self.dec_fc1(z))
         z = F.relu(self.dec_fc2(z))
@@ -73,21 +74,21 @@ class VAE(nn.Module):
         return torch.sigmoid(self.dec_fc3(z))
 
     def forward(self, x, device):
-        """フォワードメソッド（model()が呼び出されたときに実行される関数）
+        """Forward propagation
 
         Args:
-            x (torch.tensor): (バッチサイズ, 入力次元数)サイズの入力データ
-            device (String): GPUが使える場合は"cuda"でそれ以外は"cpu"
+            x (torch.tensor): Input data whose size is (batch size, x_dim).
+            device (String): "cuda" if GPU is available, or "cpu" otherwise.
 
         Returns:
-            KL (torch.float): KLダイバージェンス
-            reconstruction (torch.float): 再構成誤差
-            z (torch.tensor): (バッチサイズ, z_dim)サイズの潜在変数
-            y (torch.tensor): (バッチサイズ, 入力次元数)サイズの再構成データ            
+            KL (torch.float): KL divergence
+            reconstruction (torch.float): Reconstruction error
+            z (torch.tensor): Latent variable whose size is (Batch size, z_dim).
+            y (torch.tensor): Reconstruction data whose size is (batch size, x_dim).        
         """
-        mean, log_var = self.encoder(x.to(device)) # encoder部分
-        z = self.sample_z(mean, log_var, device) # Reparametrization trick部分
-        y = self.decoder(z) # decoder部分
-        KL = 0.5 * torch.sum(1 + log_var - mean**2 - torch.exp(log_var)) # KLダイバージェンス計算
-        reconstruction = torch.sum(x * torch.log(y + self.eps) + (1 - x) * torch.log(1 - y + self.eps)) # 再構成誤差計算
+        mean, log_var = self.encoder(x.to(device))
+        z = self.sample_z(mean, log_var, device)
+        y = self.decoder(z)
+        KL = 0.5 * torch.sum(1 + log_var - mean**2 - torch.exp(log_var))
+        reconstruction = torch.sum(x * torch.log(y + self.eps) + (1 - x) * torch.log(1 - y + self.eps))
         return [KL, reconstruction], z, y
